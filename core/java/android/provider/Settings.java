536fe9b7122310c6df30bce10bab6458db6685b8
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-13 19:54:21 +0200

    base: QS brightness slider settings: tunables -> Settings
    So we can back em up and not use tunables
    Also fix some code while at it
    
    Change-Id: I70567c0615729aacea08b8e2dfb525309e9d2872

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11209,6 +11209,7 @@ public final class Settings {
         public static final String DISABLE_SECURE_TILES_ON_LOCKSCREEN = "disable_secure_tiles_on_lockscreen";
 
         /**
+         * Whether secure QS tiles require unlocking
          * @hide
          */
         @Readable
@@ -11221,6 +11222,26 @@ public final class Settings {
          */
         @Readable
         public static final String ENABLE_LOCATION_PRIVACY_INDICATOR = "enable_location_privacy_indicator";
+
+        /**
+         * Whether to show QS brightness slider
+         * @hide
+         */
+        @Readable
+        public static final String QS_SHOW_BRIGHTNESS = "qs_show_brightness";
+
+        /**
+         * Whether to show QS brightness slider below the tiles
+         * @hide
+         */
+        @Readable
+        public static final String QS_BRIGHTNESS_POSITION_BOTTOM = "qs_brightness_position_bottom";
+
+        /**
+         * Whether to show QS auto brightness toggle button
+         * @hide
+         */
+        public static final String QS_SHOW_AUTO_BRIGHTNESS_BUTTON = "qs_show_auto_brightness_button";
 
         /**
          * These entries are considered common between the personal and the managed profile,
