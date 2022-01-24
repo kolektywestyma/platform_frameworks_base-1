733eb4fe5ff3a36e95cba7d8a1638ff95227a228
kolektywestyma <estymkakolektyw@gmail.com>
2022-01-24 15:42:38 +0100

    added @readable for sensors block

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5673,6 +5673,7 @@ public final class Settings {
         *Sensor block per-package
         * @hide
         */
+        @Readable
         public static final String SENSOR_BLOCK = "sensor_block";
 
         /**
