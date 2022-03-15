yaap-surya/frameworks_base

SystemUI: integrate smartspace and backgesture provider
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

packages/SystemUI/src/com/yaap/android/systemui/smartspace/KeyguardSmartspaceController.kt
@@ -1,0 +1,29 @@
+package com.yaap.android.systemui.smartspace
+
+import android.content.ComponentName
+import android.content.Context
+
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.statusbar.FeatureFlags
+
+import com.google.android.systemui.smartspace.KeyguardMediaViewController
+import com.google.android.systemui.smartspace.KeyguardZenAlarmViewController
+
+import javax.inject.Inject
+
+@SysUISingleton
+class KeyguardSmartspaceController @Inject constructor(
+    private val context: Context,
+    private val featureFlags: FeatureFlags,
+    private val zenController: KeyguardZenAlarmViewController,
+    private val mediaController: KeyguardMediaViewController,
+) {
+    init {
+        if (!featureFlags.isSmartspaceEnabled()) {
+            context.packageManager.setComponentEnabledSetting(ComponentName("com.android.systemui", "com.yaap.android.systemui.keyguard.KeyguardSliceProviderYaap"), 1, 1)
+        } else {
+            mediaController.init()
+            zenController.init()
+        }
+    }
+}

kolektywestyma/platform_frameworks_base-1

Change-Id: If46fe528418daca5bcd7ce2cde221c9b185947d6

packages/SystemUI/src/org/aospextended/android/systemui/smartspace/KeyguardSmartspaceController.kt
@@ -1,0 +1,29 @@
+package org.aospextended.android.systemui.smartspace
+
+import android.content.ComponentName
+import android.content.Context
+
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.statusbar.FeatureFlags
+
+import com.google.android.systemui.smartspace.KeyguardMediaViewController
+import com.google.android.systemui.smartspace.KeyguardZenAlarmViewController
+
+import javax.inject.Inject
+
+@SysUISingleton
+class KeyguardSmartspaceController @Inject constructor(
+    private val context: Context,
+    private val featureFlags: FeatureFlags,
+    private val zenController: KeyguardZenAlarmViewController,
+    private val mediaController: KeyguardMediaViewController,
+) {
+    init {
+        if (!featureFlags.isSmartspaceEnabled()) {
+            context.packageManager.setComponentEnabledSetting(ComponentName("com.android.systemui", "org.aospextended.android.systemui.keyguard.KeyguardSliceProviderAEX"), 1, 1)
+        } else {
+            mediaController.init()
+            zenController.init()
+        }
+    }
+}